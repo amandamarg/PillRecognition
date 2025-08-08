512884eee0ed41f91243054c5127e6625f855078
amandamarg <ammargo@umich.edu>
2025-08-08 18:43:17 -0400

    function for getting ranking of true label

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from glob import glob
 import os
+import torch
 
 def readLabelsObjDetect(path):
     with open(path, 'r') as f:
@@ -227,3 +228,15 @@ def generate_positive_pairs(df, labelcol="pilltype_id", enforce_ref_cons=True, e
     
     return pairs
     
+
+def true_rank(true_labels, logits):
+    assert true_labels.device.type=='cpu' and logits.device.type=='cpu'
+    batch_size, n_classes = logits.size
+    ranked_predictions = torch.argsort(logits, dim=1, descending=True)
+    ranking_mask = (ranked_predictions == true_labels.reshape(batch_size,1).expand(batch_size, n_classes))
+    rank_of_true_label = torch.argwhere(ranking_mask)
+
+    unique_labels, inv_unique_labels, freq_labels = torch.unique(true_labels, return_inverse=True, return_counts=True)
+    ordered_rankings = torch.zeros((len(unique_labels), batch_size), dtype=torch.long)
+    ordered_rankings[inv_unique_labels, rank_of_true_label[:,0]] = rank_of_true_label[:,1]
+    return rank_of_true_label, (unique_labels, ordered_rankings)
