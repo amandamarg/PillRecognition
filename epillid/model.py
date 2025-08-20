2de7d05f0984a035cee16a359fa95511d1183885
amandamarg <ammargo@umich.edu>
2025-08-20 18:57:28 -0400

    modified model to deal with two-sided inputs

--- epillid/model.py
+++ epillid/model.py
@@ -13,15 +13,24 @@ class Classifier(nn.Module):
 
 
 class MyModel(nn.Module):
-    def __init__(self, n_classes, embedding_model):
+    def __init__(self, n_classes, embedding_model, two_sided):
         super(MyModel, self).__init__()
+        self.two_sided = two_sided
         self.embedding_size = embedding_model.fc.in_features
+        #if two_sided, classifier will take in the concatenation of the embedding of the front-side image and the embedding of the back-side image
+        if self.two_sided:
+            self.embedding_size = 2*self.embedding_size
         embedding_model.fc = torch.nn.Identity()
         self.embedding_model = embedding_model
         self.classifier = Classifier(self.embedding_size, n_classes)
-    
-    def forward(self, input):
-        embeddings = self.embedding_model(input)
-        logits = self.classifier(input)
-        return embeddings, logits
 
+    def forward(self, input):
+        if self.two_sided:
+            front_embeddings = self.embedding_model(input[0])
+            back_embeddings = self.embedding_model(input[1])
+            embeddings = torch.hstack([front_embeddings, back_embeddings])
+            logits = self.classifier(embeddings)
+            return embeddings, logits
+        embeddings = self.embedding_model(input)
+        logits = self.classifier(embeddings)
+        return embeddings, logits
