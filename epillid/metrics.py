cd3d82e97aab0f9d66ac619f2e3777e3da98ddb5
amandamarg <ammargo@umich.edu>
2025-08-22 19:00:18 -0400

    more updates to metrics, started working on metrics tracker

--- epillid/metrics.py
+++ epillid/metrics.py
@@ -5,26 +5,57 @@ import torch
 import warnings
 
 class Metrics:
-    def __init__(self, best_is):
-        assert best_is in ["max", "min", 0]
-        self.best_is = best_is
+    def __init__(self, history=[], aggregation_mode=None, per_class=False, n_classes=None):
+        assert aggregation_mode in ["min", "max", "sum", "avg", None]
+        self.aggregation_mode = aggregation_mode
+        self.history = history.copy()
+        self.per_class = per_class
+        if per_class:
+            assert n_classes is not None
+            self.n_classes = n_classes
+        else:
+            self.n_classes = None
 
-    def calculate(self, hits):
+    def calculate(self, hits, labels=None, update_history=True):
         pass
+
+    def aggregate(self, metric_vals, counts=None):
+        if self.aggregation_mode == "min":
+            return metric_vals.nanmin(axis=int(self.per_class))
+        elif self.aggregation_mode == "max":
+            return metric_vals.nanmax(axis=int(self.per_class))
+        elif self.aggregation_mode == "sum":
+            return metric_vals.nansum(axis=int(self.per_class))
+        elif self.aggregation_mode == "avg":
+            if counts is None:
+                counts = len(metric_vals)
+            counts = counts - (metric_vals.isnan()).sum(axis=int(self.per_class))
+            return metric_vals.nansum(axis=int(self.per_class))/counts
+        else:
+            return metric_vals
+        
+    def get_best_val(self, return_index=True):
+        best_val = self.history.min(axis=int(self.per_class))
+        if return_index:
+            best_idx = self.history.argmin(axis=int(self.per_class))
+            return best_val, best_idx
+        return best_val
+
     
 class AP_K(Metrics):
-    def __init__(self, k, replace_nan=None, use_min=False):
-        super().__init__(best_is="max")
+    def __init__(self, k, replace_nan=None, use_min=False, history=[], aggregation_mode=None, per_class=False, n_classes=None):
+        super().__init__(history=history, aggregation_mode=None, per_class=per_class, n_classes=n_classes)
         self.k = k
-        self.use_min = use_min #if True, then for an input where cols = # columns, will use the min(cols,k) for k, otherwise if cols < k, will return None
+        self.use_min = use_min #if True, then for an input where cols = # columns, will use the min(cols,k) for k, otherwise if cols < k, will return np.nan
+        self.replace_nan = replace_nan
 
-    def calculate(self, hits):
+    def calculate(self, hits, labels=None, update_history=True):
         _,cols = hits.shape
         k = min(cols, self.k)
         if cols < self.k:
             warnings.warn("{:d} cols is less than k={:d}".format(cols,self.k))
             if not self.use_min:
-                return None
+                return np.nan
         
         top_k = hits[:,:k]
         precisions = (top_k.cumsum(axis=1) * top_k)/np.arange(1,k+1)
@@ -32,59 +63,79 @@ class AP_K(Metrics):
         N = hits.sum(axis=1)
 
         replace_val = self.replace_nan if self.replace_nan is not None else np.nan
+        metric_val = np.where(N > 0, summed_precisions/N, replace_val)
 
-        return np.where(N > 0, summed_precisions/N, replace_val)
-
+        if self.per_class:
+            grouped_labels = np.equal.outer(np.arange(self.n_classes), labels)
+            metric_vals = grouped_labels * metric_vals
+            counts = grouped_labels.sum(axis=1)
+        else:
+            counts = hits.sum()
+        
+        metric_val = self.aggregate(metric_vals, counts)
+        if update_history:
+            self.history.append(metric_val)
+        return metric_val
 
 class MAP_K(AP_K):
-    def __init__(self, k, drop_nan=True, use_min=False, per_class=False):
-        replace_nan = None if drop_nan else 0
-        super().__init__(k=k,replace_nan=replace_nan,use_min=use_min)
-        self.per_class = per_class
-        self.drop_nan = drop_nan
-
-    def calculate(self, hits, labels=None):
-        apk = super().calculate(hits=hits)
-        if apk is None:
-            return None
-        if self.drop_nan:
-            if labels is not None:
-                labels = labels[np.isnan(apk)]
-            apk = apk[np.isnan(apk)]
-        if not self.per_class:
-            return apk.sum()/len(apk)
-        unique_labels = labels.unique()
-        grouped_labels = np.equal.outer(unique_labels, labels)
-        counts = grouped_labels.sum(axis=1)
-        assert (counts > 0).all()
-        mapk = (grouped_labels @ apk)/counts
-        return (unique_labels, mapk)
-    
+    def __init__(self, k, replace_nan=None, use_min=False, history=[], per_class=False, n_classes=None):
+        super().__init__(k=k, replace_nan=replace_nan, use_min=use_min, history=history, aggregation_mode="avg", per_class=per_class, n_classes=n_classes)
 
 class MRR(Metrics):
-    def __init__(self, drop_nan=True, per_class=False):
-        super().__init__(best_is="max")
-        self.drop_nan = drop_nan
-        self.per_class = per_class
+    def __init__(self, history=[], per_class=False, n_classes=None):
+        super().__init__(history=history, aggregation_mode="avg", per_class=per_class, n_classes=n_classes)
 
-    def calculate(self, hits, labels=None):
+    def calculate(self, hits, labels=None, update_history=True):
         hits = hits & (np.cumsum(hits, axis=1) == 1) #this is to make sure we are getting the first hit in each row
         inds, top_rank = np.argwhere(hits)
         top_rank = top_rank + 1 #shift ranks so start at 1 instead of 0
-
-        if not self.per_class:
-            n = len(top_rank) if self.drop_nan else len(hits)
-            return top_rank.sum()/n
-        if self.drop_nan:
-            labels = labels[inds]
-            reciprocal_rank = 1/top_rank
+        replace_val = self.replace_nan if self.replace_nan is not None else np.nan
+        reciprocal_ranks = np.where(top_rank > 0, 1/top_rank, replace_val)
+        if self.per_class:
+            grouped_labels = np.equal.outer(np.arange(self.n_classes), labels)
+            metric_vals = grouped_labels * metric_vals
+            counts = grouped_labels.sum(axis=1)
         else:
-            reciprocal_rank = np.zeros(len(labels))
-            reciprocal_rank[inds] = (1/top_rank)
-        unique_labels = labels.unique()
-        grouped_labels = np.equal.outer(unique_labels, labels)
-        sum_rr = (grouped_labels @ reciprocal_rank)
-        counts = grouped_labels.sum(axis=1)
-        assert (counts > 0).all()
-        return (unique_labels, sum_rr/counts)
+            counts = hits.sum()
+        metric_val = self.aggregate(reciprocal_ranks, counts)
+        if update_history:
+            self.history.append(metric_val)
+        return metric_val
     
+class MetricTracker:
+    def __init__(self, logit_metrics, embedding_metrics, use_refs=False):
+        self.logit_metrics = logit_metrics.deepcopy()
+        self.embedding_metrics = embedding_metrics.deepcopy()
+        self.batch_embeddings = []
+        self.batch_logits = []
+        self.batch_labels = []
+        self.refs = [] if use_refs else None
+
+    def update_batch(self, embeddings, logits, labels, refs=None):
+        self.batch_embeddings.extend(embeddings)
+        self.batch_logits.extend(logits)
+        self.batch_labels.extend(labels)
+        if self.refs is not None and refs is not None:
+            self.refs.extend(refs)
+    
+    def clear_batch(self):
+        self.batch_embeddings = []
+        self.batch_logits = []
+        self.batch_labels = []
+        if self.refs is not None:
+            self.refs = []
+
+    def calculate_metrics(self):
+        self.batch_labels = np.array(self.batch_labels)
+        self.batch_logits = np.array(self.batch_logits)
+        self.batch_labels = np.array(self.batch_labels)
+        if self.refs is not None:
+            self.refs = np.array(self.refs)
+            ref_labels = self.batch_labels[self.refs]
+            ref_embeddings = self.batch_embeddings[self.refs]
+            cons_labels = self.batch_labels[~self.refs]
+            cons_embeddings = self.batch_embeddings[~self.refs]
+            distance_matrix = euclidean_distances(cons_embeddings, ref_embeddings)
+            sorted_distance_rankings = distance_matrix.argsort(axis=1)
+            same_labels = np.equal.outer(cons_labels, ref_labels)
+            
